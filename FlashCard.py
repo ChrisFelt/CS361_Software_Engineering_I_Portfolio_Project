8f1a66d3ec7527a6480ad094649c31cd94996d41
Chris Felt <feltc@oregonstate.edu>
2022-07-02 20:34:44 -0600

    initial commit

---
+++ FlashCard.py
@@ -0,0 +1,19 @@
+# Title: Flash Card App
+# Class: CS 361 - Software Engineering I
+# Author: Christopher Felt
+# Description: A flash card app that runs via a command prompt interface.
+
+class User:
+    pass
+
+
+class FlashCard:
+    pass
+
+
+class Collection:
+    pass
+
+
+if __name__ == '__main__':
+    pass

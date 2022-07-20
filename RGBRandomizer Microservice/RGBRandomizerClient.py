fc417d4a2c75d4fe60879b5f420635f78f9a93ed
Chris Felt <feltc@oregonstate.edu>
2022-07-20 16:42:13 -0600

    improved client interface

--- RGBRandomizer Microservice/RGBRandomizerClient.py
+++ RGBRandomizer Microservice/RGBRandomizerClient.py
@@ -13,28 +13,35 @@ print("Connecting to server...")
 socket = context.socket(zmq.REQ)
 socket.connect("tcp://localhost:7077")
 
-# generate sample request JSON
-request = {
-	"status":"run",
-	"data":
-        [
-            {"r": 180, "g": 180, "b": 180},
-            {"r": 0, "g": 255, "b": 50},
-            {"r": 180, "g": 180, "b": 180},
-            {"r": 30, "g": 55, "b": 0},
-            {"r": 71, "g": 235, "b": 50},
-            {"r": 180, "g": 180, "b": 180},
-            {"r": 30, "g": 55, "b": 0},
-        ]
-}
+# send some requests to the server
+while True:
+    # prompt user to send request JSON
+    user_input = input("\nSend a request to the RGBRandomizer microservice? Y/N: ")
 
-# send request to server
-print("Sending request...")
-socket.send_json(request)
+    if user_input.lower() == 'y':
 
-# get and print response
-response = socket.recv_json()
-print("\nReceived reply:  \n%s " % response)
+        # generate sample request JSON
+        request = {
+            "status":"run",
+            "data":
+                [
+                    {"r": 180, "g": 180, "b": 180},
+                    {"r": 0, "g": 255, "b": 50},
+                    {"r": 180, "g": 180, "b": 180},
+                    {"r": 30, "g": 55, "b": 0},
+                    {"r": 71, "g": 235, "b": 50},
+                    {"r": 180, "g": 180, "b": 180},
+                    {"r": 30, "g": 55, "b": 0},
+                ]
+        }
 
+        # send request to server
+        print("\nSending request...")
+        socket.send_json(request)
 
+        # get and print response
+        response = socket.recv_json()
+        print("\nReceived reply:  \n%s " % response)
 
+    else:
+        break
